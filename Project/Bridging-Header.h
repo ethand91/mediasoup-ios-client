7a041935fbab8b580eda9be5dd11970214930376
ethan <ethan@maru.jp>
2019-11-21 16:19:37 +0900

    Initial Commit

---
+++ Project/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  Project
+//
+//  Created by Denvir Ethan on 2019/11/20.
+//  Copyright © 2019 Denvir Ethan. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <mediasoupclient.h>
+
+#endif /* Bridging_Header_h */
---
+++ Project/ConsumerWrapper.cpp
@@ -0,0 +1,11 @@
+//
+//  ConsumerWrapper.cpp
+//  Project
+//
+//  Created by Denvir Ethan on 2019/11/21.
+//  Copyright © 2019 Denvir Ethan. All rights reserved.
+//
+
+#include "ConsumerWrapper.hpp"
+//#include "libmediasoupclient.h"
+#include "dependencies/libmediasoupclient/include/Consumer.hpp"
