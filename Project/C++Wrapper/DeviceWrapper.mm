e7b503f2160f2e58c0f48ac831b127ae4822700a
ethan <ethan@maru.jp>
2019-11-22 19:17:59 +0900

    fix: import needed frameworks etc.

---
+++ Project/C++Wrapper/DeviceWrapper.mm
@@ -0,0 +1,55 @@
+#import <Foundation/Foundation.h>
+
+#import "Device.hpp"
+
+@interface DeviceWrapper : NSObject
+@end
+
+@interface DeviceWrapper ()
+@property(atomic, readonly, assign) mediasoupclient::Device *device;
+@end
+
+@implementation DeviceWrapper
+@synthesize device = _device;
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        _device = new mediasoupclient::Device();
+    }
+    return self;
+}
+
+-(void)nativeLoad:(NSString *)routerRtpCapabilities {
+    nlohmann::json routerRtpCapabilitiesJson = nlohmann::json::parse(std::string([routerRtpCapabilities UTF8String]));
+    self.device->Load(routerRtpCapabilitiesJson);
+}
+
+-(Boolean *)nativeIsLoaded {
+    return (Boolean *)self.device->IsLoaded();
+}
+
+-(NSString *)nativeGetRtpCapabilities {
+    std::string rtpCapabilitiesString = self.device->GetRtpCapabilities().dump();
+    return [NSString stringWithUTF8String:rtpCapabilitiesString.c_str()];
+}
+
+-(Boolean *)nativeCanProduce:(NSString *)kind {
+    return (Boolean *)self.device->CanProduce(std::string([kind UTF8String]));
+}
+
+// TODO: Listener
+-(NSObject *)nativeCreateSendTransport:(NSString *)id iceParameters:(NSString *)iceParameters iceCandidates:(NSString *)iceCandidates dtlsParameters:(NSString *)dtlsParameters options:(NSString *)options appData:(NSString *)appData {
+    
+    nlohmann::json iceParametersJson = nlohmann::json::parse(std::string([iceParameters UTF8String]));
+    nlohmann::json iceCandidatesJson = nlohmann::json::parse(std::string([iceCandidates UTF8String]));
+    nlohmann::json dtlsParametersJson = nlohmann::json::parse(std::string([dtlsParameters UTF8String]));
+    
+    nlohmann::json appDataJson = nlohmann::json::object();
+    
+    if (appData != nullptr) {
+        appDataJson = nlohmann::json::parse(std::string([appData UTF8String]));
+    }
+}
+
+@end
--- Project/C++Wrapper/TransportWrapper.mm
+++ Project/C++Wrapper/TransportWrapper.mm
@@ -24,4 +24,41 @@
     return [NSString stringWithUTF8String:self.transport->GetId().c_str()];
 }
 
+-(NSString *)getNativeConnectionState {
+    return [NSString stringWithUTF8String:self.transport->GetConnectionState().c_str()];
+}
+
+-(NSString *)getNativeAppData {
+    std::string appDataString = self.transport->GetAppData().dump();
+    return [NSString stringWithUTF8String:appDataString.c_str()];
+}
+
+-(NSString *)getNativeStats {
+    std::string statsString = self.transport->GetStats().dump();
+    return [NSString stringWithUTF8String:statsString.c_str()];
+}
+
+-(Boolean *)getNativeClosed {
+    return (Boolean *)self.transport->IsClosed();
+}
+
+-(void)nativeRestartIce:(NSString *)iceParameters {
+    nlohmann::json iceParametersJson = nlohmann::json::parse(std::string([iceParameters UTF8String]));
+    self.transport->RestartIce(iceParametersJson);
+}
+
+-(void)nativeUpdateIceServers:(NSString *)iceServers {
+    nlohmann::json iceServersJson = nlohmann::json::parse(std::string([iceServers UTF8String]));
+    self.transport->UpdateIceServers(iceServersJson);
+}
+
+-(void)nativeClose {
+    self.transport->Close();
+}
+
+// TODO::
+-(void)nativeProduce:(NSObject *)track encodings:(NSString *)encodings codecOptions:(NSString *)codecOptions appData:(NSString *)appData {
+    
+}
+
 @end
