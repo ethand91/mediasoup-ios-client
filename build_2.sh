121c325b8a664a9864b9bbfcf303f8bb089e0d51
Alexander Gorbunov <gorbunov.a@vlprojects.pro>
2021-10-18 19:29:42 +0700

    Add patches needed to export missing symbols to xcframework

--- build_2.sh
+++ build_2.sh
@@ -64,6 +64,7 @@ case $INPUT_STRING in
 		patch -b -p0 -d $WORK_DIR < $PATCHES_DIR/builtin_audio_encoder_factory.patch
 		patch -b -p0 -d $WORK_DIR < $PATCHES_DIR/sdp_video_format_utils.patch
 		patch -b -p0 -d $WORK_DIR < $PATCHES_DIR/sdk_BUILD.patch
+		patch -b -p0 -d $WORK_DIR < $PATCHES_DIR/abseil_optional.patch
 		;;
 	*)
 		export PATH=$WORK_DIR/depot_tools:$PATH
